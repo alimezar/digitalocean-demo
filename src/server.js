26a4664803b31834d736b8ac25f7ec8e2d5cf9f3
Ali Sherif <ali.mezar@gmail.com>
2025-11-26 15:30:23 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const http = require('http');
 const message = process.env.ENV_MESSAGE || "No environment message set!";
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(message);
+  res.writeHead(200, {'Content-Type': 'text/plain'});;sds
+  res.end(message);a
 });
 
 server.listen(3000, () => {
