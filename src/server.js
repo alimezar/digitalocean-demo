174405ee0bd18e9c63b917a75a69e40a669326d0
Ali Sherif <ali.mezar@gmail.com>
2025-11-26 15:37:40 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,8 +7,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end(message);
 });
-
-// Only listen if this file is run directly: `node src/server.js`
+skfjew
 if (require.main === module) {
   server.listen(3000, () => {
     console.log("Server running on port 3000");
